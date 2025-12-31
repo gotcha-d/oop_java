8688acddd7f67a840062853a43a5df2420d59190
gotcha-d <gotcha.privatemail@gmail.com>
2025-12-31 11:02:53 +0900

    カードクラスの実装

--- src/main/java/org/example/oldMade/card/Card.java
+++ src/main/java/org/example/oldMade/card/Card.java
@@ -1,4 +1,77 @@
 package org.example.oldMade.card;
 
 public class Card {
+
+    private int number;
+    private int suit;
+
+    public static final int JOKER = 0;
+    public static final int SUIT_SPADE = 1;
+    public static final int SUIT_DIAMOND = 2;
+    public static final int SUIT_CLUB = 3;
+    public static final int SUIT_HEART = 4;
+
+    public Card(int number, int suit) {
+        this.number = number;
+        this.suit = suit;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    /**
+     * カードを文字列で表現する
+     *
+     * @return string
+     */
+    @Override
+    public String toString() {
+        StringBuffer string = new StringBuffer();
+
+        if (number == JOKER) {
+            string.append("JK");
+            return string.toString();
+        }
+
+        switch (suit){
+            case SUIT_SPADE:
+                string.append("S");
+                break;
+            case SUIT_DIAMOND:
+                string.append("D");
+                break;
+            case SUIT_CLUB:
+                string.append("C");
+                break;
+            case SUIT_HEART:
+                string.append("H");
+                break;
+            default:
+                break;
+        }
+
+        switch (number) {
+            case 1:
+                string.append("A");
+                break;
+            case 10:
+                string.append("T");
+                break;
+            case 11:
+                string.append("J");
+                break;
+            case 12:
+                string.append("Q");
+                break;
+            case 13:
+                string.append("K");
+                break;
+            default:
+                string.append(number);
+                break;
+        }
+
+        return string.toString();
+    }
 }
