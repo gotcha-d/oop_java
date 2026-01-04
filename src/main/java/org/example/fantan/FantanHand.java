32a35bb8b00d29e1cabcd24fdeaad7f05a5d0d29
gotcha-d <gotcha.privatemail@gmail.com>
2026-01-04 14:17:24 +0900

    7章読了

---
+++ src/main/java/org/example/fantan/FantanHand.java
@@ -0,0 +1,17 @@
+package org.example.fantan;
+
+import org.example.oldMade.card.Card;
+import org.example.oldMade.card.Hand;
+
+/**
+ * 七並べの手札クラス
+ * 間違った継承の使い方
+ */
+public class FantanHand extends Hand {
+    /**
+     * カードを見る
+     */
+    public void lookCard() {
+        // do something..
+    }
+}
